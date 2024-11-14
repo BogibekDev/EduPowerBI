4fb1b15e5820b67bc2e0c4c8897dee6f1be8b50b
BogibekDev <bogibekdev@gmail.com>
2024-11-14 07:29:26 +0500

    api updated

--- app/src/main/java/uz/ubtuit/powerbi/data/ApiClient.kt
+++ app/src/main/java/uz/ubtuit/powerbi/data/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 object ApiClient {
-    private const val BASE_URL = "https://zafarbekk1999.pythonanywhere.com/api/"
+    private const val BASE_URL = "https://zafaral.pythonanywhere.com/api/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
